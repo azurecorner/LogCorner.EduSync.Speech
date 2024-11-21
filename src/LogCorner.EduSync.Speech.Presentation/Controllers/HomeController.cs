bc29ad25f7b6a65204ee16d9fdc7438f713e3759
azurecorner <leyegora@yahoo.fr>
2024-11-21 22:48:50 +0100

    add GetWeatherForecastById

--- src/LogCorner.EduSync.Speech.Presentation/Controllers/HomeController.cs
+++ src/LogCorner.EduSync.Speech.Presentation/Controllers/HomeController.cs
@@ -29,10 +29,24 @@ namespace WebApplication2.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("{id}")]
+        public WeatherForecast? Get(int id)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Id = index,
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })?.SingleOrDefault(m => m.Id == id);
+
+        }
     }
 
     public class WeatherForecast
     {
+        public int Id { get; set; }
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
