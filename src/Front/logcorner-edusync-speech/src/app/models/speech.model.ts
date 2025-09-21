5ce032f9e887b89f5c05851676132cfe4260454c
azurecorner <leyegora@yahoo.fr>
2025-09-21 10:07:58 +0200

    create speech list

---
+++ src/Front/logcorner-edusync-speech/src/app/models/speech.model.ts
@@ -0,0 +1,21 @@
+import { SpeechType } from './SpeechType';
+
+export class Speech {
+  id: string | undefined;
+  title: string | undefined;
+  description: string | undefined;
+  url: string | undefined;
+  typeId: number | null | undefined;
+  type: SpeechType | undefined;
+  version: number | undefined;
+  constructor()
+  {
+
+  }
+}
+
+
+
+
+
+
